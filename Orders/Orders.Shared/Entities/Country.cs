0368079de99e3dda60e9d81531d10951f7cef92e
Arturo Ortiz <arturo.ortiz2@educa.madrid.org>
2024-03-19 14:28:17 +0100

    Creacion de la Base de datos y primera tablas countries

---
+++ Orders/Orders.Shared/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orders.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; } //ya tiene establecido autoIncrement
+
+        [Display(Name = "País")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caracteres.")]//maximo de caracteres
+        [Required(ErrorMessage = "El campo {0} es requerido.")]//el campo name es obligatorio
+        public String Name { get; set; } = null!;
+    }
+}
