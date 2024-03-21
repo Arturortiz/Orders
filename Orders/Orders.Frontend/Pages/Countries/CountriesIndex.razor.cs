ed69e937c21d624299ced0a15c6fc78a3ac1c6d0
Arturo Ortiz <arturo.ortiz2@educa.madrid.org>
2024-03-21 13:41:38 +0100

    Generador por defecto info del back en el front

---
+++ Orders/Orders.Frontend/Pages/Countries/CountriesIndex.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orders.Frontend.Repositories;
+using Orders.Shared.Entities;
+
+namespace Orders.Frontend.Pages.Countries
+{
+    public partial class CountriesIndex
+    {
+        [Inject] private IRepository Repository { get; set; }=null;
+        public List<Country>? Countries { get; set; }
+
+        protected async override Task OnInitializedAsync()
+        {
+            await base.OnInitializedAsync();
+            var responseHttp = await Repository.GetAsync<List<Country>>("api/countries"); //estamos yendo al backend y rellenando una lista con los paises || se pone la url del controlador del back
+            Countries = responseHttp.Response;
+        }
+    }
+}
---
+++ Orders/Orders.Frontend/Shared/GenericList.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Orders.Frontend.Shared
+{
+    public partial class GenericList<Titem>
+    {
+        [Parameter] public RenderFragment? Loading {  get; set; } //lo que yo quiero que me muestre mi componente cuando esta cargando
+        [Parameter] public RenderFragment? NoRecord {  get; set; } //lo que yo quiero que me muestre mi componente cuando no encuentra registros (paises por ejemplo)
+        [EditorRequired, Parameter] public RenderFragment Body { get; set; } = null!;// el cuerpo
+        [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null!;//la lista de productos
+    }
+}
