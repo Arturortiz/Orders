1b8848534481f52d74121a5f23a40a746e522f01
Arturo Ortiz <arturo.ortiz2@educa.madrid.org>
2024-04-16 19:56:50 +0200

    Seguridad Back parte1

--- Orders/Orders.Backend/Repositories/Implementations/UserRepository.cs
+++ Orders/Orders.Backend/Repositories/Implementations/UserRepository.cs
@@ -3,20 +3,23 @@ using Microsoft.EntityFrameworkCore;
 using Orders.Backend.Data;
 using Orders.Backend.Repositories.Interfaces;
 using Orders.Shared.Entities;
+using Orders.Shared.DTOs;
 
 namespace Orders.Backend.Repositories.Implementations
 {
     public class UsersRepository : IUsersRepository
     {
         private readonly DataContext _context;
-        private readonly UserManager<User> _userManager;
-        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<User> _userManager;//ayuda a crear usuarios
+        private readonly RoleManager<IdentityRole> _roleManager;//ayuda a crear roles
+        private readonly SignInManager<User> _signInManager;//ayuda a logearme
 
-        public UsersRepository(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        public UsersRepository(DataContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
         {
             _context = context;
             _userManager = userManager;
             _roleManager = roleManager;
+            _signInManager = signInManager;
         }
 
         public async Task<IdentityResult> AddUserAsync(User user, string password)
@@ -54,6 +57,16 @@ namespace Orders.Backend.Repositories.Implementations
         public async Task<bool> IsUserInRoleAsync(User user, string roleName)
         {
             return await _userManager.IsInRoleAsync(user, roleName);
+        }
+
+        public async Task<SignInResult> LoginAsync(LoginDTO model)
+        {
+            return await _signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);//false -> recuerde el usuario || false -> bloquee el usuario por numero de intentos fallidos
+        }
+
+        public async Task LogoutAsync()
+        {
+            _signInManager.SignOutAsync();
         }
     }
 }
---
+++ Orders/Orders.Shared/DTOs/TokenDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders.Shared.DTOs
+{
+    //esta clase va a ser la respuesta que me indica cuando yo me loguee o regsitre
+    public class TokenDTO
+    {
+        public string Token { get; set; } = null!;
+        public DateTime Expiration { get; set; }
+    }
+}
